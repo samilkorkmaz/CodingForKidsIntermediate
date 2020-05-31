46397c88ba48b9eec12cdb897e39ebc401f23396
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-05-31 21:30:22 +0300

    simplified code

--- lesson1/script.js
+++ lesson1/script.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
+ctx.rect(40, 60, 50, 70);
 ctx.fillStyle = "red";
 ctx.fill();
 ctx.closePath();
