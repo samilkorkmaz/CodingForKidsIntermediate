e95f22ef71682da470d78f6edb97fc761af3e616
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-06-02 21:48:00 +0300

    cosmetics

--- lesson1/script2.js
+++ lesson1/script2.js
@@ -12,13 +12,9 @@ ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-var cx = 240;
-var cy = 160;
-ctx.moveTo(cx, cy);
-var r = 50;
-var angleStart_rad = 0;
-var angleEnd_rad = Math.PI/2
-ctx.arc(cx, cy, r, angleStart_rad, angleEnd_rad);
-ctx.fillStyle = "green";
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "red";
+ctx.stroke();
+ctx.fillStyle = "yellow";
 ctx.fill();
 ctx.closePath();
--- lesson1/script3.js
+++ lesson1/script3.js
@@ -12,6 +12,14 @@ ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "red";
+ctx.stroke();
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
 var cx = 240;
 var cy = 160;
 ctx.moveTo(cx, cy);
@@ -21,10 +29,4 @@ var angleEnd_rad = Math.PI/2
 ctx.arc(cx, cy, r, angleStart_rad, angleEnd_rad);
 ctx.fillStyle = "green";
 ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "blue";
-ctx.stroke();
 ctx.closePath();
