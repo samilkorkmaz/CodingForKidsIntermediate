572682f9926a84f355d9ec10973efc974cc76ac3
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-06-06 12:26:26 +0300

    Move ball in x and y direction

--- lesson1/moveBall.js
+++ lesson1/moveBall.js
@@ -6,11 +6,13 @@ var r = 50;
 var angleStart_rad = 0;
 var angleEnd_rad = 2 * Math.PI
 var dy = 2;
+var dx = 2;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     cy += dy;
+    cx += dx;
     ctx.arc(cx, cy, r, angleStart_rad, angleEnd_rad);
     ctx.fillStyle = "green";
     ctx.fill();
