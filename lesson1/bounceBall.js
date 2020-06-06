22367716d8265586c1c53d9414e389555d5ae164
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-06-06 12:13:59 +0300

    Used requestAnimationFrame() instead of setInterval()

--- lesson1/bounceBall.js
+++ lesson1/bounceBall.js
@@ -5,22 +5,24 @@ var cy = 0;
 var r = 50;
 var angleStart_rad = 0;
 var angleEnd_rad = 2 * Math.PI
-const dy0 = 1;
+var dy0 = 2;
+var dy = dy0;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    if (cy + r >= canvas.height) {
+    if (cy + r >= canvas.height) { //bounce from bottom
         dy = -dy0;
     }
-    if (cy - r <= 0) {
+    /*if (cy - r <= 0) { //bounce from top
         dy = dy0;
-    }
+    }*/
     cy += dy;
     ctx.arc(cx, cy, r, angleStart_rad, angleEnd_rad);
     ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
+    window.requestAnimationFrame(draw);
 }
 
-setInterval(draw, 10);
+draw();
