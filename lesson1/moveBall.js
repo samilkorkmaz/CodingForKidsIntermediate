cf2404041e50155f9511d0a529d3c3abc4fde9a9
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-06-06 12:18:08 +0300

    Move constants to the top

--- lesson1/moveBall.js
+++ lesson1/moveBall.js
@@ -3,15 +3,14 @@ var ctx = canvas.getContext("2d");
 var cx = 240;
 var cy = 0;
 var r = 50;
+var angleStart_rad = 0;
+var angleEnd_rad = 2 * Math.PI
 var dy = 2;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     cy += dy;
-    
-    var angleStart_rad = 0;
-    var angleEnd_rad = 2 * Math.PI
     ctx.arc(cx, cy, r, angleStart_rad, angleEnd_rad);
     ctx.fillStyle = "green";
     ctx.fill();
