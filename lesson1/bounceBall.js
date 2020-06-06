2aa407d21ec444054baaa186330cf0f60162e2e6
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-06-06 12:33:24 +0300

    Add bounce from left and right

--- lesson1/bounceBall.js
+++ lesson1/bounceBall.js
@@ -7,6 +7,8 @@ var angleStart_rad = 0;
 var angleEnd_rad = 2 * Math.PI
 var dy0 = 2;
 var dy = dy0;
+var dx0 = 2;
+var dx = dx0;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -17,7 +19,14 @@ function draw() {
     if (cy - r <= 0) { //bounce from top
         dy = dy0;
     }
+    if (cx + r >= canvas.width) { //bounce from right
+        dx = -dx0;
+    }
+    if (cx - r <= 0) { //bounce from left
+        dx = dx0;
+    }
     cy += dy;
+    cx += dx;
     ctx.arc(cx, cy, r, angleStart_rad, angleEnd_rad);
     ctx.fillStyle = "green";
     ctx.fill();
