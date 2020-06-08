49983e83b5e6e980bf8d19613a030ac25907886d
Şamil Korkmaz <samil.korkmaz@gmail.com>
2020-06-08 21:39:49 +0300

    Added hitting top of brick

--- lesson3/hitBricks.js
+++ lesson3/hitBricks.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById("myCanvas"); var ctx = canvas.getContext("2d");
-var defColor = "blue"; var brickWidth = 50; var brickHeight = 15; var bricks = [];
-var radius = 7; var cx = radius; var cy = canvas.height - radius;
-var dx0 = 2; var dx = dx0; var dy0 = -2; var dy = dy0;
+nRow = 3; nCol = 7; var defColor = "blue";
+var brickWidth = Math.floor(canvas.width / nCol / 10) * 10; var brickHeight = 15; var bricks = [];
+var radius = 4; var cx = radius; var cy = canvas.height - radius;
+var dx0 = 4; var dx = dx0; var dy0 = -4; var dy = dy0;
 var bricksHit = 0;
 
 function start() {
@@ -10,9 +11,15 @@ function start() {
 }
 
 function createBricks() {
-    for (let i = 0; i < 7; i++) {
-        var brick = { w: brickWidth, h: brickHeight, x: 10 + i * (brickWidth + 5), y: 60, color: defColor};
-        bricks.push(brick);
+    for (let iRow = 0; iRow < nRow; iRow++) {
+        for (let iCol = 0; iCol < nCol; iCol++) {
+            var brick = {
+                w: brickWidth, h: brickHeight, 
+                x: 10 + iCol * (brickWidth + 5),
+                y: 170 + iRow * (brickHeight + 10), color: defColor
+            };
+            bricks.push(brick);
+        }
     }
 }
 
@@ -24,17 +31,25 @@ function animate() {
     for (let i = 0; i < bricks.length; i++) {
         ctx.beginPath();
         ctx.rect(bricks[i].x, bricks[i].y, bricks[i].w, bricks[i].h);
-        if (cx >= bricks[i].x && cx <= bricks[i].x + bricks[i].w &&
-            cy + radius >= bricks[i].y && cy - radius <= bricks[i].y + bricks[i].h) {
-            if (bricks[i].color === defColor) bricksHit++;
-            bricks[i].color = "red";
-            dy = -dy0; //bounce
+        if (bricks[i].color !== "lightGray") { //check collision
+            if (cx >= bricks[i].x && cx <= bricks[i].x + bricks[i].w) {
+                if (cy - radius <= bricks[i].y + bricks[i].h && cy + radius >= bricks[i].y + bricks[i].h) { //hit brick from bottom
+                    if (bricks[i].color === defColor) bricksHit++;
+                    bricks[i].color = "lightGray"; //make brick invisible
+                    dy = -dy0; //bounce downward
+                } else if (cy + radius >= bricks[i].y && cy-radius <= bricks[i].y) { //hit brick from top
+                    if (bricks[i].color === defColor) bricksHit++;
+                    bricks[i].color = "lightGray"; //make brick invisible
+                    dy = dy0; //bounce upward
+                }
+            }
+            ctx.fillStyle = bricks[i].color;
+            ctx.fill();
+            ctx.closePath();
         }
-        ctx.fillStyle = bricks[i].color;
-        ctx.fill();
-        ctx.closePath();
     }
     if (cy + radius >= canvas.height) dy = dy0; //bounce from bottom
+    if (cy - radius <= 0) dy = -dy0; //bounce from top
     if (cx + radius >= canvas.width) dx = -dx0; //bounce from right
     if (cx - radius <= 0) dx = dx0; //bounce from left
     window.requestAnimationFrame(animate);
@@ -45,7 +60,7 @@ function moveBall(cx, cy) {
     var angleStart_rad = 0;
     var angleEnd_rad = 2 * Math.PI
     ctx.arc(cx, cy, radius, angleStart_rad, angleEnd_rad);
-    if(bricksHit === bricks.length) {
+    if (bricksHit === bricks.length) {
         ctx.fillStyle = "red";
     } else {
         ctx.fillStyle = "green";
