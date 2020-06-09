336a4e4c20a8c3e767a06a2fe1da8a1a1a4c4bb6
samil korkmaz <samil.korkmaz@gmail.com>
2020-06-09 17:17:10 +0300

    Started adding checkHit()

--- lesson3/hitBricksAdvanced.js
+++ lesson3/hitBricksAdvanced.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("myCanvas"); var ctx = canvas.getContext("2d");
-nRow = 3; nCol = 7; var defColor = "blue";
+nRow = 3; nCol = 7; var defBrickColor = "blue";
 var brickWidth = Math.floor(canvas.width / nCol / 10) * 10; var brickHeight = 15; var bricks = [];
 var radius = 4; var cx = radius; var cy = canvas.height - radius;
 var dx0 = 4; var dx = dx0; var dy0 = -4; var dy = dy0;
@@ -16,7 +16,9 @@ function createBricks() {
             var brick = {
                 w: brickWidth, h: brickHeight,
                 x: 10 + iCol * (brickWidth + 5),
-                y: 70 + iRow * (brickHeight + 10), color: defColor
+                y: 70 + iRow * (brickHeight + 10),
+                color: defBrickColor,
+                isNotHit: true
             };
             bricks.push(brick);
         }
@@ -29,31 +31,45 @@ function animate() {
     cy += dy;
     moveBall(cx, cy);
     for (let i = 0; i < bricks.length; i++) {
-        ctx.beginPath();
         bricks[i].y += 0.1;
-        ctx.rect(bricks[i].x, bricks[i].y, bricks[i].w, bricks[i].h);
-        if (bricks[i].color !== "lightGray") { //check collision
+        if (bricks[i].isNotHit) { //check collision
             if (cx >= bricks[i].x && cx <= bricks[i].x + bricks[i].w) {
                 if (cy - radius <= bricks[i].y + bricks[i].h && cy + radius >= bricks[i].y + bricks[i].h) { //hit brick from bottom
-                    if (bricks[i].color === defColor) bricksHit++;
-                    bricks[i].color = "lightGray"; //make brick invisible
+                    bricksHit++;
+                    bricks[i].isNotHit = false;
                     dy = -dy0; //bounce downward
                 } else if (cy + radius >= bricks[i].y && cy - radius <= bricks[i].y) { //hit brick from top
-                    if (bricks[i].color === defColor) bricksHit++;
-                    bricks[i].color = "lightGray"; //make brick invisible
+                    bricksHit++;
+                    bricks[i].isNotHit = false;
                     dy = dy0; //bounce upward
                 }
             }
+        }
+        if (bricks[i].isNotHit) { //don't draw hit bricks because their lightgray color will obscure the ball
+            ctx.beginPath();
+            ctx.rect(bricks[i].x, bricks[i].y, bricks[i].w, bricks[i].h);
             ctx.fillStyle = bricks[i].color;
             ctx.fill();
             ctx.closePath();
         }
     }
+    //Collision with canvas:
     if (cy + radius >= canvas.height) dy = dy0; //bounce from bottom
     if (cy - radius <= 0) dy = -dy0; //bounce from top
     if (cx + radius >= canvas.width) dx = -dx0; //bounce from right
     if (cx - radius <= 0) dx = dx0; //bounce from left
     window.requestAnimationFrame(animate);
+}
+
+function checkHit(isNotHit, cx, cy, radius, x, y, w, h) {
+    if (isNotHit) {
+        //TODO Add brick collision logic here
+    }
+    //Collision with canvas:
+    if (cy + radius >= canvas.height) dy = dy0; //bounce from bottom
+    if (cy - radius <= 0) dy = -dy0; //bounce from top
+    if (cx + radius >= canvas.width) dx = -dx0; //bounce from right
+    if (cx - radius <= 0) dx = dx0; //bounce from left
 }
 
 var ballTrail = [];
